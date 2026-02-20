82cdb7a585f0da46f3a37e71bac32e50931d81b8
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2026-02-20 23:06:14 +0500

    Fix build errors

--- src/lib/services/config/index.ts
+++ src/lib/services/config/index.ts
@@ -1,15 +1,13 @@
-import { logger } from "@/lib/helpers";
-import { LoggerLevel } from "@/utils/enums";
-
 export const getConfig = () => {
-    const { NEXT_PUBLIC_BACKEND_URL, NEXT_PUBLIC_NODE_ENV, NEXT_PUBLIC_GRAPHQL_URL } = process.env;
-    if (!NEXT_PUBLIC_BACKEND_URL || !NEXT_PUBLIC_NODE_ENV || !NEXT_PUBLIC_GRAPHQL_URL) {
-        logger({ type: LoggerLevel.ERROR, message: "NEXT_PUBLIC_BACKEND_URL is not set" });
+    const { NEXT_PUBLIC_BACKEND_URL, NEXT_PUBLIC_NODE_ENV, NEXT_PUBLIC_BACKEND_GRAPHQL_URL } = process.env;
+    console.log("ENV VARS", NEXT_PUBLIC_BACKEND_URL, NEXT_PUBLIC_NODE_ENV, NEXT_PUBLIC_BACKEND_GRAPHQL_URL);
+    if (!NEXT_PUBLIC_BACKEND_URL || !NEXT_PUBLIC_NODE_ENV || !NEXT_PUBLIC_BACKEND_GRAPHQL_URL) {
+        console.error("NEXT_PUBLIC_BACKEND_URL is not set");
         throw new Error("NEXT_PUBLIC_BACKEND_URL is not set");
     }
     return {
         BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-        GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+        GRAPHQL_URL: process.env.NEXT_PUBLIC_BACKEND_GRAPHQL_URL,
         NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
     } as const
 }
