032340ced42f189effadba00d9be6d7fdc972b08
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2026-04-26 00:48:10 +0500

    upgrade landing page

--- src/lib/ui/useable-components/sidebar/index.tsx
+++ src/lib/ui/useable-components/sidebar/index.tsx
@@ -116,7 +116,7 @@ function SidebarProvider({
             } as CSSProperties
           }
           className={cn(
-            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
+            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh min-w-0 w-full overflow-x-clip",
             className
           )}
           {...props}
@@ -280,7 +280,7 @@ function SidebarInset({ className, ...props }: MainComponentProps) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        "bg-background relative flex w-full flex-1 flex-col",
+        "bg-background relative flex min-w-0 w-full flex-1 flex-col",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
         className
       )}
